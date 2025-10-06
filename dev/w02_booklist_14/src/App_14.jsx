045f7031a7a47aab411ae263c06a98d79d24e551
zero2005x <zero2005x@gmail.com>
2025-10-06 10:56:45 +0800

    refactor: Improve code formatting and consistency in App_14.jsx

--- dev/w02_booklist_14/src/App_14.jsx
+++ dev/w02_booklist_14/src/App_14.jsx
@@ -1,11 +1,17 @@
-import './App_14.scss';
-import books_data from './booklist_data';
+import "./App_14.scss";
+import books_data from "./booklist_data";
 
-const App_14 = () => <Booklist_14 />;
+const App_14 = () => {
+  return (
+    <>
+      <Booklist_14 />;
+    </>
+  );
+};
 
 const Booklist_14 = () => {
   return (
-    <section className='booklist'>
+    <section className="booklist">
       {books_data.map((book) => {
         const { id, img, title, author } = book;
         return <Book_14 key={id} img={img} title={title} author={author} />;
@@ -16,9 +22,9 @@ const Booklist_14 = () => {
 
 const Book_14 = ({ img, title, author }) => {
   return (
-    <article className='book'>
-      <img src={img} alt={title} />
-      <div className='bookinfo'>
+    <article className="book">
+      <img src={img} />
+      <div className="bookinfo">
         <h1>{title}</h1>
         <h4>{author}</h4>
       </div>
