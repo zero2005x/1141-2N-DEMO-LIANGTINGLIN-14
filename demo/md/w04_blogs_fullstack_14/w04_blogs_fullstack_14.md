490bc51c195c6a43c6c0848721fd19a70efc9767
zero2005x <zero2005x@gmail.com>
2025-10-08 20:54:57 +0800

    Add server and database setup for blog application
    - Created package.json for server_blogs_14 with necessary dependencies and scripts.
    - Implemented server.js to handle API requests and serve blog data.
    - Added SQL script (blog_14.sql) to create blog table and insert sample data.
    - Developed database.js to manage database connections for both local and Supabase environments.

---
+++ demo/md/w04_blogs_fullstack_14/w04_blogs_fullstack_14.md
@@ -0,0 +1,49 @@
+[Github URL](https://github.com/zero2005x/1141-2N-DEMO-LIANGTINGLIN-14)
+
+### W04-P1: Create a express web server to show your info
+
+![](w04-p1-1.png)
+
+```
+cffb896 zero2005x       Wed Oct 8 18:51:20 2025 +0800   W04-P1: Create a express web server to show your info
+```
+
+### W04-P2: Create blog_xx table with 3 data, implement route /api/blog_xx to return a json array with 3 data
+
+#### => SQL to create blog_xx table and 3 data
+
+![](w04-p2-1.png)
+
+#### => show 3 data
+
+![](w04-p2-2.png)
+
+#### => implement route /api/blog_xx
+
+![](w04-p2-3.png)
+
+```
+893ac84 zero2005x       Wed Oct 8 19:52:38 2025 +0800   W04-P2: Create blog_xx table with 3 data, implement route /api/blog_xx to return a json array with 3 data
+```
+
+### W04-P3: from client side to get json data from Node
+
+#### => modified client and server code
+
+![](w04-p3-1.png)
+
+#### => Chrome, show 3 blogs
+
+![](w04-p3-2.png)
+
+```
+ed89ef0 zero2005x       Wed Oct 8 20:47:12 2025 +0800   W04-P3: from client side to get json data from Node
+```
+
+## W04-logs: git logs of w04
+
+![](w04-logs.png)
+
+```
+
+```
---
+++ dev/w04_blogs_fullstack_14/client_blogs_14/src/page/BlogNodePage_14.jsx
@@ -0,0 +1,200 @@
+import { FaGlobe } from "react-icons/fa";
+import { FaMugSaucer } from "react-icons/fa6";
+
+import Blog_14 from "../components/Blog_14";
+import { useState, useEffect } from "react";
+
+const api_url = "http://localhost:3000/api/blogs_14";
+
+const BlogNodePage_14 = () => {
+  const [name, setName] = useState("liangtinglin");
+  const [id, setID] = useState(913410014);
+  const [blogs_14, setBlogs_14] = useState([]);
+
+  const fetchBlogFromNodeServer = async () => {
+    try {
+      const response = await fetch(api_url);
+      const data = await response.json();
+      console.log("data from node server", data);
+      setBlogs_14(data);
+    } catch (error) {
+      console.log("fetchBlogFromNodeServer error", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchBlogFromNodeServer();
+  }, []);
+
+  return (
+    <>
+      <section className="blogs">
+        <div className="section-title">
+          <h2>
+            Blog from Node Page -- {name}, {id}
+          </h2>
+        </div>
+        <div className="blogs-center">
+          {blogs_14.map((item) => {
+            const { id, img, category, title, desc } = item;
+            return (
+              <Blog_14
+                key={id}
+                id={id}
+                img={img}
+                category={category}
+                title={title}
+                desc={desc}
+              />
+            );
+          })}
+        </div>
+      </section>
+
+      <section className="blogs">
+        <div className="section-title">
+          <h2>CSS Grid using minmax</h2>
+        </div>
+        <div className="blogs-center2">
+          <article className="blog">
+            <img
+              src="./images/photo-1.jpg"
+              alt="Coffee photo"
+              className="img blog-img"
+            />
+            <div className="blog-content">
+              <span>
+                lifestyle <FaMugSaucer />
+              </span>
+              <h3>seven reasons why coffee is awesome</h3>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+              <a href="#">read more</a>
+            </div>
+          </article>
+          <article className="blog">
+            <img
+              src="./images/photo-2.jpg"
+              alt="Coffee photo"
+              className="img blog-img"
+            />
+            <div className="blog-content">
+              <span>
+                lifestyle <FaGlobe />
+              </span>
+              <h3>travel to paris</h3>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+              <a href="#">read more</a>
+            </div>
+          </article>
+          <article className="blog">
+            <img
+              src="./images/photo-3.jpg"
+              alt="Coffee photo"
+              className="img blog-img"
+            />
+            <div className="blog-content">
+              <span>
+                lifestyle <FaMugSaucer />
+              </span>
+              <h3>Coffee Brings Friendship</h3>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+              <a href="#">read more</a>
+            </div>
+          </article>
+          <article className="blog">
+            <img
+              src="./images/photo-4.jpg"
+              alt="Coffee photo"
+              className="img blog-img"
+            />
+            <div className="blog-content">
+              <span>
+                lifestyle <FaMugSaucer />
+              </span>
+              <h3>Coffee Makes You Feel Good</h3>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+              <a href="#">read more</a>
+            </div>
+          </article>
+          <article className="blog">
+            <img
+              src="./images/photo-5.jpg"
+              alt="Coffee photo"
+              className="img blog-img"
+            />
+            <div className="blog-content">
+              <span>
+                lifestyle <FaMugSaucer />
+              </span>
+              <h3>Coffee Make You Calm</h3>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+              <a href="#">read more</a>
+            </div>
+          </article>
+          <article className="blog">
+            <img
+              src="./images/photo-6.jpg"
+              alt="Coffee photo"
+              className="img blog-img"
+            />
+            <div className="blog-content">
+              <span>
+                lifestyle <FaGlobe />
+              </span>
+              <h3>101 Tower In Taipei</h3>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+              <a href="#">read more</a>
+            </div>
+          </article>
+          <article className="blog">
+            <img
+              src="./images/photo-7.jpg"
+              alt="Coffee photo"
+              className="img blog-img"
+            />
+            <div className="blog-content">
+              <span>
+                lifestyle <FaGlobe />
+              </span>
+              <h3>Sun Rise From The Mountain</h3>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+              <a href="#">read more</a>
+            </div>
+          </article>
+          <article className="blog">
+            <img
+              src="./images/photo-8.jpg"
+              alt="Coffee photo"
+              className="img blog-img"
+            />
+            <div className="blog-content">
+              <span>
+                lifestyle <FaGlobe />
+              </span>
+              <h3>Serene Lake With Trees</h3>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+              <a href="#">read more</a>
+            </div>
+          </article>
+          <article className="blog">
+            <img
+              src="./images/photo-9.jpg"
+              alt="Coffee photo"
+              className="img blog-img"
+            />
+            <div className="blog-content">
+              <span>
+                lifestyle <FaGlobe />
+              </span>
+              <h3>Rocks Of Queen Head In Yehliu Taiwan</h3>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+              <a href="#">read more</a>
+            </div>
+          </article>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default BlogNodePage_14;
